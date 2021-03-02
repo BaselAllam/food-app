b41d491aa0962fa21882e576881bb817183e3533
basselallam <baseljahen@gmail.com>
2021-03-02 21:53:15 +0200

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:food/screens/homepage.dart';
+
+void main() => runApp(MyApp());
 
 
-void main() {
-  return runApp(FirstSceen());
-}
 
 
-class FirstSceen extends StatelessWidget{
-
-final String x = 'Welcome to our screen';
-
-@override
+class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return Text(x);
-  }
-}
-
-
-
-
-class SecondScreen extends StatefulWidget{
-
-  @override
-  _SecondScreenState createState() => _SecondScreenState();
-}
-
-class _SecondScreenState extends State<SecondScreen>{
-
-String x = 'Welcome to state';
-
-  @override
-  Widget build(BuildContext context){
-    return Text(x);
+    return MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
